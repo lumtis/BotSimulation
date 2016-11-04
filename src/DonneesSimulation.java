e93b72b4f8f3a6569991a3b1a808f764080aab06
Bertrand <bertrand@wificampus-097209.grenet.fr>
2016-11-04 13:52:33 +0100

    donneesimulation

---
+++ src/DonneesSimulation.java
@@ -0,0 +1,14 @@
+
+
+
+public class DonneesSimulation
+{
+    private Carte carte;
+    private Incendie[] incendies;
+    private Robot[] robots;
+
+    public void DonneesSimulation(String name)
+    {
+        carte = new Carte();
+    }
+}
