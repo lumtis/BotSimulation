c949d7aa100923b8a2215a98c99f4ce9a6695639
Thami Benjelloun <Thami.benjelloun@ensimag.grenoble-inp.fr>
2016-11-14 17:44:00 +0100

    djiktra

--- src/simulation/Case.java
+++ src/simulation/Case.java
@@ -7,10 +7,19 @@ public class Case {
     private int colonne;
     private NatureTerrain nature;
 
+    /**
+     * \brief compare deux cases
+     * \return true si le cases sont égales, false si non
+     * \param case 1
+     * \param case 2
+     */
     public static boolean casesEgales(Case c1, Case c2) {
     	return ((c1.ligne == c2.ligne) && (c1.colonne == c2.colonne));
     }
 
+    /**
+     * \brief constructeur case
+     */
     public Case()
     {
       this.ligne = 0;
@@ -18,6 +27,13 @@ public class Case {
       this.nature = NatureTerrain.TERRAIN_LIBRE;
     }
 
+
+    /**
+     * \brief constructeur case
+     * \param ligne de la case
+     * \param colonne de la case
+     * \param nature de la case
+     */
     public Case(int ligne, int colonne, NatureTerrain nature)
     {
         this.ligne = ligne;
@@ -25,16 +41,31 @@ public class Case {
         this.nature = nature;
     }
 
+
+    /**
+     * \brief
+     * \ return la ligne de la cae
+     */
     public int getLigne()
     {
         return ligne;
     }
 
+    /**
+     * \brief
+     * \ return la colonne de la case
+     */
     public int getColonne()
     {
         return colonne;
     }
 
+
+
+    /**
+     * \brief
+     * \ return la nature du terrain
+     */
     public NatureTerrain getNature()
     {
         return nature;
